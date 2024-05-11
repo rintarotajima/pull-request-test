9ddeccad4ba1d023cfe421739b63b1f61b7bd918
rintarotajima <rintarotajima@gmail.com>
2024-05-11 16:35:47 +0900

    配列をソートする処理を追加しました

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
+var sortNumber = function (number) {
+    number.sort(function (a,b) {
+        if (a == b) {
+            return 0;
+        }
+        return a < b ? -1 : 1;
+    })
+}
+
 var number = [10, 35, 2, 59, 89, 56];
+sortNumber(number);
 console.log(number);
